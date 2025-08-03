d0f091d3af8c738ae97e3488e7aa358db1a541f5
Paula <paula.emanuelef.dantas@gmail.com>
2025-08-02 22:18:43 -0300

    Implementa uma função para agregar amigos

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 // variável do tipo array que armazenará os nomes dos amigos inseridos.
 
 let listaDeAmigos = [];
+
+// função para adicionar amigos
+
+function adicionarAmigo() {
+    // capturar o valor do campo de entrada
+
+    let amigo = document.getElementById('amigo').value;
+    
+    // validação para garantir que o campo não esteja vazio
+
+    if (amigo === '') {
+        alert('Por favor, insira um nome.');
+    }
+    else {  // valor válido
+        listaDeAmigos.push(amigo);
+        console.log(listaDeAmigos)
+    }
+
+    // Limpar o campo de entrada após adicionar o nome
+    
+    amigo = document.getElementById('amigo');
+    amigo.value = '';
+}
